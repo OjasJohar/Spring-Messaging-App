dc372c0178a1e08ea6db8abb92ba338d156d825a
OjasJohar <ojasjohar@gmail.com>
2025-02-22 11:23:13 +0530

    [Ojas] Add. created GET Request Method and pass name as path variable

--- Spring_MessagingApp/src/main/java/com/bridgelabz/Spring_MessagingApp/controller/HelloRestController.java
+++ Spring_MessagingApp/src/main/java/com/bridgelabz/Spring_MessagingApp/controller/HelloRestController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.Spring_MessagingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,12 @@ public class HelloRestController {
     //UC2
     @GetMapping("/hello/query")
     public String printHello(@RequestParam(value = "name") String name){
+        return "Hello " + name;
+    }
+
+    //UC3
+    @GetMapping("/hello/param/{name}")
+    public String printHelloParam(@PathVariable String name){
         return "Hello " + name;
     }
 
